432c9e23465ea7449727981b81b3b767cfe55a4c
Abhradip Roy <abhra0312@gmail.com>
2025-04-25 12:51:28 +0530

    ..

--- backend/src/utils/passport.ts
+++ backend/src/utils/passport.ts
@@ -1,62 +1,57 @@
 import passport from "passport";
-import {Strategy as GoogleStrategy} from "passport-google-oauth2" // googleStrategy as it is google oauth
-import {userModel} from "../Models/DB_MODEL.js"
+import { Strategy as GoogleStrategy } from "passport-google-oauth2"; // googleStrategy as it is google oauth
+import { userModel } from "../Models/DB_MODEL.js";
 
+export const configurePassport = async () => {
+  console.log("✅ CALLBACK URL:", process.env.OAUTH_CALLBACK_URL);
 
+  // setting the strategy configuration for passport
 
-
-
-
-
-export const configurePassport = async()=>{
-
-
-
-console.log("✅ CALLBACK URL:", process.env.OAUTH_CALLBACK_URL);
-
-
-// setting the strategy configuration for passport
-
-    passport.use(new GoogleStrategy({
+  passport.use(
+    new GoogleStrategy(
+      {
         clientID: process.env.OAUTH_CLIENT_ID || "",
         clientSecret: process.env.OAUTH_CLIENT_SECRET || "",
-        callbackURL: process.env.OAUTH_CALLBACK_URL || ""
-    },async(accessToken:string,refreshToken:string,profile:any,done:any)=>{
-            try {
-                const user = await userModel.findOne({email: profile.emails[0].value}) // searching databse to find if the user already exists in the database
-                if(!user){
-                   const user = await userModel.create({
-                        googleId:profile.id,
-                        name:profile.displayName,
-                        email:profile.emails[0].value,
-                        avatar:profile.photos[0].value
-                    });
-                }
-                else if (!user.googleId) {
-                    user.googleId = profile.id;
-                    await user.save();
-                }
-                return done(null,user)
-            } catch (error) {
-                return done(error,null)
-            }
-    }))
-
-
-    passport.serializeUser((user:any,done)=>done(null,user._id)) //serializing users
-
-    passport.deserializeUser(async(id:any,done)=>{   // deserializing user to get the data out of it 
+        callbackURL: process.env.OAUTH_CALLBACK_URL || "",
+      },
+      async (
+        accessToken: string,
+        refreshToken: string,
+        profile: any,
+        done: any
+      ) => {
         try {
-            const user = await userModel.findById(id);
-            done(null, user); // makes `req.user` available in routes
-          } catch (err) {
-            done(err, null);
+          let user = await userModel.findOne({
+            email: profile.emails[0].value,
+          }); // searching databse to find if the user already exists in the database
+          if (!user) {
+             user = await userModel.create({
+              googleId: profile.id,
+              name: profile.displayName,
+              email: profile.emails[0].value,
+              avatar: profile.photos[0].value,
+            });
+          } else if (!user.googleId) {
+            user.googleId = profile.id;
+            await user.save();
           }
-    })
+          return done(null, user);
+        } catch (error) {
+          return done(error, null);
+        }
+      }
+    )
+  );
 
-}
+  passport.serializeUser((user: any, done) => done(null, user._id)); //serializing users
 
-
-
-
-
+  passport.deserializeUser(async (id: any, done) => {
+    // deserializing user to get the data out of it
+    try {
+      const user = await userModel.findById(id);
+      done(null, user); // makes `req.user` available in routes
+    } catch (err) {
+      done(err, null);
+    }
+  });
+};
