6cb2cb37d3a7f9fe8a9c7fec8fff69dbcf1d41db
Abhradip Roy <abhra0312@gmail.com>
2025-04-27 11:44:18 +0530

    ..

--- backend/src/Router/userRoutes.ts
+++ backend/src/Router/userRoutes.ts
@@ -19,19 +19,19 @@ const userRoutes = Router()
 
 // Route for getting the data of the user that is currently logged in
 
-userRoutes.get("/currentuser",(req:Request,res:Response)=>{
-    if(req.isAuthenticated()){
-        res.status(200).json({
-            success:true,
-            msg:req.user
-        })
-    }else{
-        res.status(401).json({
-            success:false,
-            msg:"The user is not authenticated"
-        })
-    }
-})
+// userRoutes.get("/currentuser",(req:Request,res:Response)=>{
+//     if(req.isAuthenticated()){
+//         res.status(200).json({
+//             success:true,
+//             msg:req.user
+//         })
+//     }else{
+//         res.status(401).json({
+//             success:false,
+//             msg:"The user is not authenticated"
+//         })
+//     }
+// })
 
 
 export default userRoutes
