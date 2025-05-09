2171e790339646d97adf1e884fd241bbb28d49ee
Abhradip Roy <abhra0312@gmail.com>
2025-05-09 19:02:52 +0530

    ..

--- frontend/src/MyComponent/Desktop/DesktopComponent/Collection.tsx
+++ frontend/src/MyComponent/Desktop/DesktopComponent/Collection.tsx
@@ -90,10 +90,10 @@ function Collection({
       {musicList.map(({ _id, audio_URL, audio_name, img_URL }) => (
            <div
            key={_id}
-           className=" basis-1/3 h-[100px] border-box p-[10px] border-r-[2px] border-white bg-amber-300 "
+           className=" basis-1/3 h-[100px] border-box p-[10px] border-r-[2px] border-white bg-amber-300  "
            onClick={() => handleTrack("")}
          >
-          <span>{audio_name}</span>
+          <span className="w-full overflow-clip ">{audio_name}</span>
           <span>{img_URL}</span>
           <span>{audio_URL}</span>
          </div>
