12696edbe7a6c58a1b294e932e2e54ca0fbb4228
Abhradip Roy <abhra0312@gmail.com>
2025-05-09 22:14:41 +0530

    ..

--- backend/src/middleware/AdminCheck.ts
+++ backend/src/middleware/AdminCheck.ts
@@ -1,34 +1,35 @@
 //Middleware for checking if admin or not
 
-import { Request,Response,NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
 
 // Extend the Request interface to include isAdmin
 declare global {
-    namespace Express {
-        interface Request {
-            isAdmin?: boolean;
-        }
+  namespace Express {
+    interface Request {
+      isAdmin?: boolean;
     }
+  }
 }
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
-
-export const isAdmin = (req:Request,res:Response,next:NextFunction)=>{
-    const isToken = req.headers.isAdmin
-    if (!isToken) {
-        return res.status(401).json({ msg: "Admin token missing in 'isAdmin' header" });
-      }
-    try{let token
-    if(isToken){
-        token = isToken
+export const isAdmin = (req: Request, res: Response, next: NextFunction) => {
+  const isToken = req.headers.isAdmin;
+  if (!isToken) {
+   console.log("token not ")
+  }
+  try {
+    let token;
+    if (isToken) {
+      token = isToken;
     }
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     if (decoded && typeof decoded !== "string") {
-        req.isAdmin = (decoded as jwt.JwtPayload).isAdmin;
+      req.isAdmin = (decoded as jwt.JwtPayload).isAdmin;
     }
-    next()}catch(err){
-        res.status(403).json({
-            msg:"Not authorized"
-        })
-    }
-}
+    next();
+  } catch (err) {
+    res.status(403).json({
+      msg: "Not authorized",
+    });
+  }
+};
